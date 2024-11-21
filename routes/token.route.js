9d4a99c0ac0c07bed7c6be39f049ce05a4f37be5
phamdat <dat@undercurrent.tech>
2024-11-21 20:38:58 +0700

    get token txs

--- routes/token.route.js
+++ routes/token.route.js
@@ -3,8 +3,7 @@ const router =express.Router();
 const tokenCtrl = require('../controllers/token.controller');
 
 /*---Token Balance--*/
-// router.route('/price').get(tokenCtrl.get_xau_price)
-// router.route('/txs').get(tokenCtrl.get_tx_by_addr)
 router.route('/info').get(tokenCtrl.token_info)
+router.route('/txs').get(tokenCtrl.token_tx)
 
 module.exports = router
